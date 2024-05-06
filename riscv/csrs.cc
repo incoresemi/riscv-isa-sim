af42e34b32bf38692c37a4ba051763ede925c22a
Puneet Goel <puneet@coverify.com>
2024-05-06 16:10:05 +0530

    Allow MISA CSR P bit to be modified

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -631,7 +631,7 @@ bool sstatus_csr_t::enabled(const reg_t which) {
 misa_csr_t::misa_csr_t(processor_t* const proc, const reg_t addr, const reg_t max_isa):
   basic_csr_t(proc, addr, max_isa),
   max_isa(max_isa),
-  write_mask(max_isa & (0  // allow MABFDQCHV bits in MISA to be modified
+  write_mask(max_isa & (0  // allow MABFDQCHVPU bits in MISA to be modified
                         | (1L << ('M' - 'A'))
                         | (1L << ('A' - 'A'))
                         | (1L << ('B' - 'A'))
@@ -640,6 +640,7 @@ misa_csr_t::misa_csr_t(processor_t* const proc, const reg_t addr, const reg_t ma
                         | (1L << ('Q' - 'A'))
                         | (1L << ('C' - 'A'))
                         | (1L << ('H' - 'A'))
+                        | (1L << ('P' - 'A'))
                         | (1L << ('U' - 'A'))
                         | (1L << ('V' - 'A'))
                         )
